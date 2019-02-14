3bcab81b795fb84cdfaf5aae52d5513ce555b223
Jakob Weisblat <jakob@keyba.se>
2019-02-14 13:02:19 -0500

    wip

--- go/kbfs/libkbfs/conflict_resolver.go
+++ go/kbfs/libkbfs/conflict_resolver.go
@@ -3339,7 +3339,7 @@ func (cr *ConflictResolver) recordFinishResolve(
 		thisCR.ErrorString = fmt.Sprintf("%+v", receivedErr)
 	}
 	if panicVar != nil {
-		thisCR.PanicString = fmt.Sprintf("panic(%s). stack: %b", panicVar,
+		thisCR.PanicString = fmt.Sprintf("panic(%s). stack: %s", panicVar,
 			debug.Stack())
 	}
 
