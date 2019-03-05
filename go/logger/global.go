0e5da85fa0b61e8de7fd327bd3c96757dee53a17
Jakob Weisblat <jakob@keyba.se>
2019-03-05 17:28:57 -0500

    logger: use bufio.NewWriter to buffer logs
    Issue: KBFS-3941

--- go/logger/global.go
+++ go/logger/global.go
@@ -1,8 +1,11 @@
 package logger
 
 import (
+	"bufio"
+	"io"
 	"os"
 	"sync"
+	"time"
 
 	logging "github.com/keybase/go-logging"
 	isatty "github.com/mattn/go-isatty"
@@ -12,7 +15,21 @@ var globalLock sync.Mutex
 var stderrIsTerminal = isatty.IsTerminal(os.Stderr.Fd())
 var currentLogFileWriter *LogFileWriter
 
+func getBufferedErrorWriter() io.Writer {
+	writer := ErrorWriter()
+	buf := bufio.NewWriter(writer)
+	go func() {
+		for range time.Tick(300 * time.Millisecond) {
+			// TODO: do we care if this errors
+			buf.Flush()
+		}
+	}()
+	return buf
+}
+
 func init() {
-	logBackend := logging.NewLogBackend(ErrorWriter(), "", 0)
+	// TODO: I am not sure how color gets set on the logger,
+	//  but it's not logging in color anymore sometimes
+	logBackend := logging.NewLogBackend(getBufferedErrorWriter(), "", 0)
 	logging.SetBackend(logBackend)
 }
