aa6695a5c24eba68ef862b52d60f608587d8964b
Jakob Weisblat <jakob@keyba.se>
2019-03-08 18:03:36 -0500

    logger: make buffered logging thread safe

--- go/logger/buffer.go
+++ go/logger/buffer.go
@@ -6,11 +6,15 @@ package logger
 import (
 	"bufio"
 	"io"
+	"sync"
 	"time"
 )
 
 type autoFlushingBufferedWriter struct {
-	bufferedWriter   *bufio.Writer
+	lock           sync.RWMutex
+	bufferedWriter *bufio.Writer
+	backupWriter   *bufio.Writer
+
 	frequency        time.Duration
 	timer            *time.Timer
 	currentlyWaiting bool
@@ -23,11 +27,20 @@ func (writer *autoFlushingBufferedWriter) backgroundFlush() {
 	for {
 		select {
 		case <-writer.timer.C:
-			// TODO: I claim this is concurrency safe, but how do I tell the race
-			//  checker that?
+			// Swap out active and backup writers
+			writer.lock.Lock()
 			writer.currentlyWaiting = false
-			writer.bufferedWriter.Flush()
+			previouslyActive := writer.bufferedWriter
+			writer.bufferedWriter = writer.backupWriter
+			writer.lock.Unlock()
+
+			// Flush the previously active writer and complete the swap. We do
+			// not need to complete the swap under lock because this is the
+			// only goroutine accessing backupWriter.
+			writer.backupWriter = previouslyActive
+			writer.backupWriter.Flush()
 		case <-writer.shutdown:
+			writer.timer.Stop()
 			writer.bufferedWriter.Flush()
 			return
 		}
@@ -38,9 +51,9 @@ func (writer *autoFlushingBufferedWriter) backgroundFlush() {
 // and flushes automatically after `flushFrequency`.
 func NewAutoFlushingBufferedWriter(baseWriter io.Writer,
 	flushFrequency time.Duration) (w io.Writer, shutdown chan struct{}) {
-	bufferedWriter := bufio.NewWriter(baseWriter)
 	result := &autoFlushingBufferedWriter{
-		bufferedWriter: bufferedWriter,
+		bufferedWriter: bufio.NewWriter(baseWriter),
+		backupWriter:   bufio.NewWriter(baseWriter),
 		frequency:      flushFrequency,
 		timer:          time.NewTimer(flushFrequency),
 	}
@@ -49,6 +62,8 @@ func NewAutoFlushingBufferedWriter(baseWriter io.Writer,
 }
 
 func (writer *autoFlushingBufferedWriter) Write(p []byte) (int, error) {
+	writer.lock.RLock()
+	defer writer.lock.RUnlock()
 	n, err := writer.bufferedWriter.Write(p)
 	if err != nil {
 		return n, err
@@ -56,8 +71,17 @@ func (writer *autoFlushingBufferedWriter) Write(p []byte) (int, error) {
 	if writer.currentlyWaiting {
 		return n, nil
 	}
-	// TODO: I think this is concurrency safe
+	// Docs say this should not be called concurrently with channel receive.
+	// I've read the internal code and the only 2 ways it could resolve are
+	// that the timer is stopped when it has not yet fired, or it fires and
+	// then is reset. In the latter case, we get slightly more frequent
+	// flushing than we need; in the former, we get a slight delay. This race
+	// is unlikely, so it's exceedingly unlikely that the delay will accumulate
+	// over more than one successive race.
 	writer.timer.Reset(writer.frequency)
+	// Any race over `currentlyWaiting` will have minimal impact. The only race
+	// possible given the locking is between two copies of the below line
+	// both trying to set the value to `true` simultaneously.
 	writer.currentlyWaiting = true
 
 	return n, nil
